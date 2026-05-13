5b631de3271a15b26a7fcfc04102ccfc32ee1331
Long <vuminhl@sheridancollege.ca>
2026-05-13 04:08:26 -0400

    Refactor the split_data.py to use the labels in train_test_split as well. This effectively reduce the confusion of image extension

--- split_data.py
+++ split_data.py
@@ -17,12 +17,16 @@ def split_data(images_dir,labels_dir, percent_val=0.15,percent_test=0.15):
     percent_train: the percentarge of the train part
     percent_val: the percentage of validation part
     percent_test: the percentage of test part
-    return: return True if successful
+    return: 3 images and 3 labels subsets for train, val, and test, respectively
     '''
     images = list()
+    labels = list()
     for img in listdir(images_dir):
         # only take the name not extension
-        images.append(img.split(".")[0])
+        images.append(img)
+    for label in listdir(labels_dir):
+        
+        labels.append(label)
 
     '''
     we will perform two splits
@@ -48,58 +52,73 @@ def split_data(images_dir,labels_dir, percent_val=0.15,percent_test=0.15):
     '''
     
     # First split
-    train_val, test, _, _ = train_test_split(
-    images, images, 
+    train_val_images, test_images, train_val_labels, test_labels = train_test_split(
+    images, labels, 
     test_size=percent_test, 
     random_state=42
     )
     val_size = percent_val / (1 - percent_test)
-    train, val, _, _ = train_test_split(
-    train_val, train_val, 
+    train_images, val_images, train_labels, val_labels  = train_test_split(
+    train_val_images, train_val_labels, 
     test_size= val_size, 
     random_state=42
     )
-    return train, val, test
+    return train_images, val_images, test_images, train_labels, val_labels, test_labels
 
-def copy_images_to_dest(src_dir, dest_dir, src_files):
+def copy_files_to_dest(src_dir, dest_dir, src_files):
+    
+    '''
+    copy_files_to_dest: copy selected files in a directory to a location
+    src_dir: the source directory contains all the files
+    dest_dir: the destination directory
+    src_files: the selected files in that directory 
+    return: no return
+    '''
+    
+    
     # there might be mess up between jpg and png 
     os.makedirs(os.path.dirname(dest_dir), exist_ok=True)
     for fl in src_files:
-        
-        fl_src = f"{src_dir}\\{fl}.png"
-        if( os.path.isfile(fl_src)):
-            fl_dest = f"{dest_dir}\\{fl}.png"
-        else:
-            fl_src = f"{src_dir}\\{fl}.jpg"
-            fl_dest = f"{dest_dir}\\{fl}.jpg"
-        shutil.copyfile(fl_src, fl_dest)
-def copy_labels_to_dest(src_dir, dest_dir, src_files):
-    os.makedirs(os.path.dirname(dest_dir), exist_ok=True)
-    for fl in src_files:
-        fl_src = f"{src_dir}\\{fl}.txt"
-        fl_dest = f"{dest_dir}\\{fl}.txt"
+        fl_dest = f"{dest_dir}\\{fl}"
+        fl_src = f"{src_dir}\\{fl}"
         shutil.copyfile(fl_src, fl_dest)
 
 def copy_images_labels_to_dest(images_dir,labels_dir, dest):
-    train, val, test = split_data(images_dir,images_dir)
+    
+    '''
+    copy_images_labels_to_dest: this application will split the data into the YOLO format
+    where it include train,val, and test inside data directory. Each train,val,and test itself contains
+    images and labels folders
+    images_dir: the path to the images folder
+    labels_dir: the path to the labels folder
+    dest: the destination where we put the YOLO formated daata
+    return: no return
+    '''
+    
+    
+    train_images, val_images, test_images, train_labels, val_labels, test_labels\
+        = split_data(images_dir, labels_dir)
     train_images_dir = dest + "data/train/images/"
     train_labels_dir = dest + "data/train/labels/"
+    
     val_images_dir = dest + "data/val/images/"
     val_labels_dir = dest + "data/val/labels/"
+    
     test_images_dir = dest + "data/test/images/"
     test_labels_dir = dest + "data/test/labels/"
     
-    copy_images_to_dest(images_dir, train_images_dir, train)
-    copy_labels_to_dest(labels_dir, train_labels_dir, train)
+    copy_files_to_dest(images_dir, train_images_dir, train_images)
+    copy_files_to_dest(labels_dir, train_labels_dir, train_labels)
+
+    copy_files_to_dest(images_dir, val_images_dir, val_images)
+    copy_files_to_dest(labels_dir, val_labels_dir, val_labels)
     
-    copy_images_to_dest(images_dir, val_images_dir, val)
-    copy_labels_to_dest(labels_dir, val_labels_dir, val)
-    
-    copy_images_to_dest(images_dir, test_images_dir, test)
-    copy_labels_to_dest(labels_dir, test_labels_dir, test)
+    copy_files_to_dest(images_dir, test_images_dir, test_images)
+    copy_files_to_dest(labels_dir, test_labels_dir, test_labels)
     
     
-images_dir = r"C:\Users\haivu\OneDrive - Sheridan College\Desktop\computer\Project\Face_Recognition\images"
-labels_dir = r"C:\Users\haivu\OneDrive - Sheridan College\Desktop\computer\Project\Face_Recognition\labels"
+    
+images_dir = r"C:\Users\haivu\Downloads\train_yolo\images"
+labels_dir = r"C:\Users\haivu\Downloads\train_yolo\labels"
 
 copy_images_labels_to_dest(images_dir,labels_dir,"./")
