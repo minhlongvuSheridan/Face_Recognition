e2980e47a39bc8eb826a548e2f0213cc0f68f331
Long <vuminhl@sheridancollege.ca>
2026-05-15 21:46:25 -0400

    Refactor draw_box.py to add neon effects by using Gaussian blur. Refactor main and facenet_no_sort for better constants

--- draw_box.py
+++ draw_box.py
@@ -3,73 +3,206 @@ Author: Minh Long Vu
 Application: This functions draw the customized bounding box instead of traditional rectangle one
 '''
 
+import time
+import math
 import cv2
 
+# ------------------------------- Neon effect ------------------------------------
+def find_center_image(img):
+    width = img.shape[0]
+    height = img.shape[1]
+    x_center = width // 2
+    y_center = height // 2
+    return (x_center, y_center)
 
-def draw_corners(image, x1, y1, x2, y2, length, thick):
+def get_points_for_connector(img,x1,y1,x2,y2, length, rect_width, rect_height):
+    x_center = (x1 + x2) / 2
+    y_center = (y1 + y2) / 2
+    center = (x_center, y_center)
+    delta_x = int(length * math.cos(math.pi /4 ))
+    delta_y = int(length * math.sin(math.pi /4 ))
     
-    '''
-    draw_corners: Basically this draw the side of rectangle but instead of one full side from corner
-    to corner. It only draw two short line from a corner
-    image: an numpy array representing image
-    x1: horizontal position of top right corner point (x1,y1)
-    y1: vertical position of top right corner point (x1,y1)
-    x2: horizontal position of top right corner point (x2,y2)
-    y2: vertical position of top right corner point (x2,y2)
-    length: the length of that short line. If you specify x2 - x1 then it is basically
-    traditional side
-    thick: the thickness of each line
-    return: No return
-    '''
+    
+    img_center = find_center_image(img)
+    if center[0] > img_center[0]  and center[1] > img_center[1]:
+        box_point = (x1,y1)
+        # bottom right
+        rect_point = (box_point[0] - delta_x, box_point[1] - delta_y)
+        # bottom left
+        bottom_left = (rect_point[0] - rect_width,rect_point[1])
+    elif center[0] > img_center[0] and  center[1] < img_center[1]:
+        box_point = (x1,y2)
+        # top right
+        rect_point = (box_point[0] - delta_x, box_point[1] + delta_y)
+        # bottom left
+        bottom_left = (rect_point[0] - rect_width, rect_point[1] + rect_height)
+        
+    elif center[0] < img_center[0]  and center[1] > img_center[1] : 
+        box_point = (x2,y1)
+        # bottom left
+        rect_point = (box_point[0] + delta_x, box_point[1] - delta_y)
+        # bottom left
+        bottom_left = rect_point
+    else:
+        box_point = (x2, y2)
+        # tio left
+        rect_point = (box_point[0] + delta_x, box_point[1] + delta_y)
+        # bottom left
+        bottom_left = (rect_point[0], rect_point[1] + rect_height)
+    return box_point, rect_point, bottom_left
+
+def get_width_height_text(Name, Job, Country, text_size):
+    (Name_width, Name_Height), _ = cv2.getTextSize(Name,cv2.FONT_HERSHEY_SIMPLEX, text_size, 1)
+    (Job_width, _), _ = cv2.getTextSize(Job,cv2.FONT_HERSHEY_SIMPLEX, text_size, 1)
+    (Country_width, _), _ = cv2.getTextSize(Country,cv2.FONT_HERSHEY_SIMPLEX, text_size, 1)
+    max_width = Name_width
+    if max_width < Job_width:
+        max_width = Job_width
+    if max_width < Country_width:
+        max_width = Country_width
+    print(f"name: {Name_width} job: {Job_width} country {Country_width}")
+    return max_width, Name_Height
+
+def get_points_for_text(rec_bottom_left, text_height, spacing, padding):
+    
+    country_point = (rec_bottom_left[0] + padding,
+                     rec_bottom_left[1] - padding)
+    job_point = (country_point[0],
+                 country_point[1] - text_height - spacing)
+    name_point = (job_point[0],
+                  job_point[1] - text_height - spacing)
+    return name_point, job_point, country_point
+
+
+def draw_neon_corners(image, x1, y1, x2, y2, inner_color,outer_color, neon_strength,length, thick):
+    
+    start = time.time()
     # rename for shorter word
     l = length
     t = thick
     green_retro = (44,255,5)
+    mask_neon = image.copy()
+    mask_neon[:] = (0,0,0) # didn't affect anything if blend
+    # Step 2: Apply blurred text, the bigger the more blurred
     
-    # Top Left
-    cv2.line(image, (x1, y1), (x1 + l,y1), green_retro, t )
-    cv2.line(image, (x1, y1), (x1,y1 + l), green_retro, t )
+    for i in range(thick + 2* neon_strength, thick, -2):
+        # top left
+        cv2.line(mask_neon, (x1, y1),(x1 + l,y1), outer_color, i)
+        cv2.line(mask_neon, (x1, y1), (x1,y1 + l), outer_color, i)
+        # top right
+        cv2.line(mask_neon, (x2, y1), (x2 - l, y1), outer_color, i)
+        cv2.line(mask_neon, (x2, y1), (x2, y1 + l), outer_color, i)
+        # bottom left
+        cv2.line(mask_neon, (x1, y2), (x1 + l, y2), outer_color, i)
+        cv2.line(mask_neon, (x1, y2), (x1, y2 - l), outer_color, i)
+        
+        # bottom right
+        cv2.line(mask_neon, (x2, y2), (x2 - l,y2), outer_color, i)
+        cv2.line(mask_neon, (x2, y2), (x2,y2 - l), outer_color, i)
+        # at certain pixel, it will look at surrounding (x,y) the bigger x and y are, the more blurred it will be 
+        mask_neon = cv2.GaussianBlur(mask_neon,(2*i +1, 2*i + 1), 0)
+    # Step 3 - Blend with the image
+    # why it need to be one?
+    
+    cv2.addWeighted(mask_neon,0.7, image, 1,0,image)
+    # Step 4 - write the usual white text
+    
+    # Top left
+    cv2.line(image, (x1, y1), (x1 + l,y1), inner_color,thick)
+    cv2.line(image, (x1, y1), (x1,y1 + l), inner_color,thick)
     
     # Top Right
-    cv2.line(image, (x2, y1), (x2 - l, y1), green_retro, t )
-    cv2.line(image, (x2, y1), (x2, y1 + l), green_retro, t )
-    
+    cv2.line(image, (x2, y1), (x2 - l, y1), inner_color,thick)
+    cv2.line(image, (x2, y1), (x2, y1 + l), inner_color,thick)
     # Bottom Left
-    cv2.line(image, (x1, y2), (x1 + l, y2), green_retro, t )
-    cv2.line(image, (x1, y2), (x1, y2 - l), green_retro, t )
-    
+    cv2.line(image, (x1, y2), (x1 + l, y2), inner_color,thick)
+    cv2.line(image, (x1, y2), (x1, y2 - l), inner_color,thick)
     # Bottom Right
-    cv2.line(image, (x2, y2), (x2 - l,y2), green_retro, t )
-    cv2.line(image, (x2, y2), (x2,y2 - l), green_retro, t )
+    cv2.line(image, (x2, y2), (x2 - l,y2), inner_color,thick)
+    cv2.line(image, (x2, y2), (x2,y2 - l), inner_color,thick)
+    
+
+    end = time.time()
+    print(f"Corner took {end-start}")
 
 
-def draw_label(image, x1, y1,label, color_text, color_box):
+def draw_neon_label(image, x1, y1,x2, y2, label, 
+                    inner_text_color,
+                    outer_text_color,
+                    text_strength, 
+                    text_size,
+                    bg_box_color,
+                    inner_border_color,
+                    outer_border_color,
+                    border_strength,
+                    border_thick):
+    style_text = cv2.FONT_HERSHEY_SIMPLEX
+    Name = f"{label["name"]}"
+    Job = f"{label["job"]}"
+    Country = f"{label["country"]}"
+    text_width, text_height = get_width_height_text(Name, Job, Country, text_size)    
+    line_spacing = 4
+    padding = 8
+    connector_length = 20
     
-    '''
-    draw_label: this draw the label together with rectangle surrounding it. The reason why
-    we only take (X1,y1) is because we want to draw on the top. You could customize by using
-    (x2,y2) if you want to draw in the bottom
-    image: an numpy array representing image
-    x1 - Int: horizontal position of top right corner point (x1,y1)
-    y1 - Int: vertical position of top right corner point (x1,y1)
-    label - String: What text do you want to display as a label for the object
-    color_text - (Int,Int,Int): tuple of three RGB specifying the color of label
-    color_box - (Int,Int,Int): tuple of three RGB specifying the color of label
-    '''
+    rec_width = text_width + 2 * padding
+    rec_height = 3 * text_height + 2 * padding + 2 * line_spacing
+    box_point, rect_point, bottom_left = get_points_for_connector(
+                                                    image, 
+                                                    x1,
+                                                    y1,
+                                                    x2,
+                                                    y2, 
+                                                    connector_length,
+                                                    rec_width,
+                                                    rec_height)
+
     
     
+
+    name_start, job_start, country_start = get_points_for_text(bottom_left,
+                                                               text_height,
+                                                               line_spacing,
+                                                               padding)
+    # Fill in the label box
     overlay = image.copy()
-    # We want to fit the label inside the rectangle so we need to know the size of label
-
-    (text_width, text_height), _ = cv2.getTextSize(label,cv2.FONT_HERSHEY_SIMPLEX, 0.8, 1)
     
-    # we need to use blending techquie: combine many images together 
-    # if we want to add transparent rectangle
-    cv2.rectangle(overlay,(x1 - 5,y1 - 15 + 10 ),(x1 + text_width + 5,y1 - text_height - 15 - 10), color_box,-1)
-    # dst = alpha * overlay + beta * image + gamma
-    # dst: write to where
-    # addWeighted(overlay, alpha, image, beta, gamma, dst)
+    cv2.rectangle(overlay,
+                  (bottom_left[0], bottom_left[1] - rec_height),
+                  (bottom_left[0] + rec_width, bottom_left[1]), 
+                  (0,60,0), -1)
+  
     cv2.addWeighted(overlay,0.6,image,1- 0.6,0,image) 
-    # Note that if we want to make it feel like on top. the coffeificent should larger than
-    # 0.5 which means it is more dense
-    cv2.putText(image,label, (x1, y1 - 15),cv2.FONT_HERSHEY_SIMPLEX, 0.8,  color_text, 1)
+    
+    # Draw neon corners and label
+    
+    mask_neon = image.copy()
+    mask_neon[:] = (0,0,0) # didn't affect anything if blend
+    # Step 2: Apply blurred text, the bigger the more blurred
+    for i in range(border_thick + border_strength, 1, -1):
+        # rectangle
+        cv2.rectangle(mask_neon, 
+                      (bottom_left[0], bottom_left[1] - rec_height), 
+                      (bottom_left[0] + rec_width, bottom_left[1]), 
+                      outer_border_color, i)
+        # line connector
+        cv2.line(mask_neon, box_point,rect_point, outer_border_color, i) 
+        # text
+        cv2.putText(mask_neon,Name, name_start,style_text, text_size ,outer_text_color, i)
+        cv2.putText(mask_neon,Job, job_start,style_text, text_size ,outer_text_color, i)
+        cv2.putText(mask_neon,Country, country_start,style_text, text_size ,outer_text_color, i)
+        
+        # blur elements
+        mask_neon = cv2.GaussianBlur(mask_neon,(2*i +1, 2*i + 1), 0)
+    # Step 3 - Blend with the image
+    cv2.addWeighted(mask_neon,0.7, image, 1,0,image)
+    # Step 4 - write the usual white text
+    cv2.rectangle(image, 
+                  (bottom_left[0], bottom_left[1] - rec_height),
+                  (bottom_left[0] + rec_width, bottom_left[1]), 
+                  inner_border_color,
+                  1)
+    cv2.line(image, box_point,rect_point, inner_border_color, 1)
+    cv2.putText(image,Name, name_start,style_text, text_size ,inner_text_color, 1)
+    cv2.putText(image,Job, job_start,style_text, text_size ,inner_text_color, 1)
+    cv2.putText(image,Country, country_start,style_text, text_size ,inner_text_color, 1)
--- facenet_no_sort.py
+++ facenet_no_sort.py
@@ -7,7 +7,6 @@ from draw_box import *
 from sort import *
 from load_data import *
 import time
-
 WHITE = (255,255,255) 
 GREY = (128, 128, 128) # Medium Grey for background
 LIGHT_GREEN =  (150, 255, 150) # lighter is closer to white so it is INNER
@@ -60,15 +59,15 @@ track_dict = dict()
 # FaceNet
 embedder = FaceNet()
 # the path to the images for recognition
-dir_path = r'./identity'
+dir_path = r'C:\Users\haivu\OneDrive - Sheridan College\Desktop\computer\Project\Face_Recognition\identity'
 train_faces, train_labels = load_dataset_faces(dir_path)
 embeded_faces = list()
 embeded_faces = embedder.embeddings(train_faces) # Extract features vector
 in_encoder = Normalizer(norm='l2') # 
 X = in_encoder.transform(embeded_faces) # normalize the feature vectors
 out_encoder = LabelEncoder()
-out_encoder.fit(train_labels) # learn the unique class and then assign them a numerical value to create mapping
-Y = out_encoder.transform(train_labels) # actually perform the conversion based on what it learn before
+out_encoder.fit(train_labels) # encode the string to proper format for SVM
+Y = out_encoder.transform(train_labels)
 
 # SVM model
 SVM_model = SVC(kernel='linear', probability=True)
@@ -84,6 +83,8 @@ while True:
         continue
     Detections = np.empty((0, 5))
     results = model(img, stream = True)
+    to_embbeded_images = []
+    to_embbeded_metadata = []
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -92,58 +93,34 @@ while True:
             length = side_length // 6
             
             conf = int(float(box.conf[0]) * 100)
+            
             if conf >= 50:
-                currentArray = np.array([x1, y1, x2, y2, conf])
-                Detections = np.vstack((Detections, currentArray))
-            
-    tracker_results = tracker.update(Detections)
-    to_embbeded_images = []
-    to_embbeded_metadata = []
-    for tracker_result in tracker_results:
-        x1, y1, x2, y2, id = map(int, tracker_result)
-        # if negative then convert it to zero
-        x1, y1, x2, y2 = [max(0, i) for i in (x1, y1, x2, y2)]
-        if id in track_dict:
-            name = track_dict.get(id)[0]
-            name_info= dict()
-            name_info["name"] = name
-            name_info["job"] = names_information[name]["job"]
-            name_info["country"] = names_information[name]["country"]
-            draw_neon_corners(img, x1, y1, x2, y2,
-                            LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
-            
-            draw_neon_label(img, x1, y1, x2, y2, name_info, 
-                            LIGHT_GREEN, MEDIUM_GREEN,TEXT_STRENGTH, TEXT_SIZE,
-                            GREY, LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH,BORDER_THICK)
-        else:
-            sub_image = img.copy()[y1:y2,x1:x2]
-            to_embbeded_images.append(sub_image)
-            to_embbeded_metadata.append((x1, y1, x2, y2, id))
+                sub_image = img.copy()[y1:y2,x1:x2]
+                to_embbeded_images.append(sub_image)
+                to_embbeded_metadata.append((x1, y1, x2, y2))
     if len(to_embbeded_images) > 0:
         embedded_images = embedder.embeddings(to_embbeded_images)
         for i, embeded_image in enumerate(embedded_images): 
-            (x1, y1, x2, y2, id) = to_embbeded_metadata[i]
+            (x1, y1, x2, y2) = to_embbeded_metadata[i]
             normalized_sub_feat = in_encoder.transform([embeded_image])
             preds = SVM_model.predict_proba(normalized_sub_feat)
             best_class_idx = np.argmax(preds[0])
             probability = preds[0][best_class_idx] * 100
             name = out_encoder.inverse_transform([best_class_idx])[0]
-            if(probability >= 60):
-                track_dict[id] = [name, probability]
-                name_info= dict()
-                name_info["name"] = name
-                print(name)
-                name_info["job"] = names_information[name]["job"]
-                name_info["country"] = names_information[name]["country"]
-                draw_neon_corners(img, x1, y1, x2, y2,
-                                  LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
-                draw_neon_label(img, x1, y1, x2, y2, name_info, 
-                                LIGHT_GREEN, MEDIUM_GREEN,TEXT_STRENGTH, TEXT_SIZE,
-                                GREY, LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH,BORDER_THICK)
+            name_info= dict()
+            name_info["name"] = name
+            print(name)
+            name_info["job"] = names_information[name]["job"]
+            name_info["country"] = names_information[name]["country"]
+            draw_neon_corners(img, x1, y1, x2, y2,
+                                LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
+            draw_neon_label(img, x1, y1, x2, y2, name_info, 
+                            LIGHT_GREEN, MEDIUM_GREEN,TEXT_STRENGTH, TEXT_SIZE,
+                            GREY, LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH,BORDER_THICK)
     
     end_time = time.time()
     elapsed_time = end_time - start_time  
-    cv2.putText(img, f"{1 // elapsed_time} FPS",(20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (1, 50, 32), 1)      
+    cv2.putText(img, f"{1 // elapsed_time} FPS",(50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (1, 50, 32), 1)      
     cv2.imshow("Image", img)
     cv2.waitKey(1)
     
