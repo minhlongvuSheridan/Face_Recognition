efee894879436626701f2db12ad4588b99da9ba0
Long <vuminhl@sheridancollege.ca>
2026-05-18 14:26:13 -0400

    refactor the main function using threading. No more lagging. This is actually real power of tracking

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Date: 17-05-2026
 
 
 
+import threading
+
 import cv2
 from sklearn.preprocessing import LabelEncoder, Normalizer
 from sklearn.svm import SVC
@@ -31,6 +33,7 @@ BORDER_STRENGTH = 1
 BORDER_THICK = 1
 
 names_information = {
+    
     "Minh Long Vu":{
         "job": "Student",
         "country": "Canada"
@@ -58,6 +61,9 @@ names_information = {
 }
 
 
+
+
+
 # YOLO model
 model = YOLO("./detection.pt") 
 
@@ -81,6 +87,15 @@ Y = out_encoder.transform(train_labels) # actually perform the conversion based 
 # SVM model
 SVM_model = SVC(kernel='linear', probability=True)
 SVM_model.fit(X, Y)
+
+embeded_images_id = [True, dict()]
+def recognition_threading(to_embbeded_images, to_embedded_id):
+    embedded_images = embedder.embeddings(to_embbeded_images)
+    for i, image in enumerate(embedded_images):
+        id = to_embedded_id[i]
+        embeded_images_id[1][id] = image
+    embeded_images_id[0] = True
+
 
 
 lap_camera = cv2.VideoCapture(0)
@@ -106,49 +121,60 @@ while True:
             
     tracker_results = tracker.update(Detections)
     to_embbeded_images = []
-    to_embbeded_metadata = []
+    to_embbeded_id = []
     for tracker_result in tracker_results:
         x1, y1, x2, y2, id = map(int, tracker_result)
         # if negative then convert it to zero
         x1, y1, x2, y2 = [max(0, i) for i in (x1, y1, x2, y2)]
         if id in track_dict:
-            name = track_dict.get(id)[0]
-            name_info= dict()
-            name_info["name"] = name
-            name_info["job"] = names_information[name]["job"]
-            name_info["country"] = names_information[name]["country"]
-            draw_neon_corners(img, x1, y1, x2, y2,
-                            LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
-            
-            draw_neon_label(img, x1, y1, x2, y2, name_info, 
-                            LIGHT_GREEN, MEDIUM_GREEN, TEXT_SIZE,
-                            BACKGROUND_GREEN, LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH,BORDER_THICK)
-        else:
-            sub_image = img.copy()[y1:y2,x1:x2]
-            to_embbeded_images.append(sub_image)
-            to_embbeded_metadata.append((x1, y1, x2, y2, id))
-    if len(to_embbeded_images) > 0:
-        embedded_images = embedder.embeddings(to_embbeded_images)
-        for i, embeded_image in enumerate(embedded_images): 
-            (x1, y1, x2, y2, id) = to_embbeded_metadata[i]
-            normalized_sub_feat = in_encoder.transform([embeded_image])
-            preds = SVM_model.predict_proba(normalized_sub_feat)
-            best_class_idx = np.argmax(preds[0])
-            probability = preds[0][best_class_idx] * 100
-            name = out_encoder.inverse_transform([best_class_idx])[0]
-            if(probability >= 60):
-                track_dict[id] = [name, probability]
+            if(track_dict[id][0] == False):
+                name = track_dict.get(id)[3]
                 name_info= dict()
                 name_info["name"] = name
                 print(name)
                 name_info["job"] = names_information[name]["job"]
                 name_info["country"] = names_information[name]["country"]
                 draw_neon_corners(img, x1, y1, x2, y2,
-                                  LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
+                                LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH, length, BORDER_THICK)
+                
                 draw_neon_label(img, x1, y1, x2, y2, name_info, 
                                 LIGHT_GREEN, MEDIUM_GREEN, TEXT_SIZE,
                                 BACKGROUND_GREEN, LIGHT_GREEN, MEDIUM_GREEN, BORDER_STRENGTH,BORDER_THICK)
+        else:
+            point1 = (x1,x2)
+            point2 = (y1,y2)
+            track_dict[id] = [True, point1, point2, "Unknow"]
+            sub_image = img.copy()[y1:y2,x1:x2]
+            to_embbeded_images.append(sub_image)
+            to_embbeded_id.append((id))
     
+    
+    if embeded_images_id[0] == True:
+        for key, value in embeded_images_id[1].items():
+            id = key
+            embbeded_image = value
+            normalized_sub_feat = in_encoder.transform([embbeded_image])
+            preds = SVM_model.predict_proba(normalized_sub_feat)
+            best_class_idx = np.argmax(preds[0])
+            probability = preds[0][best_class_idx] * 100
+    
+            if(probability >= 50):
+                name = out_encoder.inverse_transform([best_class_idx])[0]
+                track_dict[id][0] = False
+                track_dict[id][3] = name
+            else: 
+                to_embbeded_images.append(sub_image)
+                to_embbeded_id.append((id))
+        embeded_images_id[0] = False
+        embeded_images_id[1].clear()
+            
+            
+            
+    # we don't want to change the size of embeded_images
+    if len(to_embbeded_images) > 0 and embeded_images_id[0] == False:
+        embeded_images_id[0] = False
+        threading.Thread(target=recognition_threading, args=(to_embbeded_images,to_embbeded_id,)).start()
+
     end_time = time.time()
     elapsed_time = end_time - start_time  
     cv2.putText(img, f"{1 // elapsed_time} FPS",(20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (1, 50, 32), 1)      
